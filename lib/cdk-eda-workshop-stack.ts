b209d6fe12063497899212b25bb5e9d5f2dc9aaf
Seoyeon Park <seoyeonp@amazon.com>
2023-01-31 10:49:12 +0900

    ecs-pattern to ECS

--- lib/cdk-eda-workshop-stack.ts
+++ lib/cdk-eda-workshop-stack.ts
@@ -9,6 +9,8 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { IpAddresses } from 'aws-cdk-lib/aws-ec2';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager'
 import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Duration } from 'aws-cdk-lib';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EdaWSNetworkStack extends Stack {
@@ -138,37 +140,65 @@ export class EdaWSEcsStack extends Stack {
     // ECS Cluster, Task Definition, ALB
     const cluster = new ecs.Cluster(this, "EdaEcsCluster", {
         vpc: vpc,
-        clusterName: 'ecs-cluster-coffee'
+        clusterName: 'eda-ws-cluster-coffee'
     });
 
-
-    const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'EdaWSFargateSvc', {
-      cluster,
-      serviceName: 'ecs-svc-eda-coffee-app',
-      loadBalancerName: 'lb-eda-ws-coffee',
+    const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'ApiTaskDefinition', {
       memoryLimitMiB: 1024,
       cpu: 512,
-      desiredCount: 2,
-      taskImageOptions: {
-        image: ecs.ContainerImage.fromEcrRepository(repository),
-        environment: {
-          SPRING_PROFILES_ACTIVE: "dev",
-        },
-        containerPort: 8080,
-        containerName: 'coffee-app'
-      },
-      taskSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-      },
-      targetProtocol: ApplicationProtocol.HTTP,
+      family: 'eda-ws-task'
     });
-    
-    loadBalancedFargateService.taskDefinition.addToExecutionRolePolicy(ecsExecutionPolicy);
-    loadBalancedFargateService.taskDefinition.addToTaskRolePolicy(ecsTaskPolicy);
+    fargateTaskDefinition.addToExecutionRolePolicy(ecsExecutionPolicy);
+    fargateTaskDefinition.addToTaskRolePolicy(ecsTaskPolicy);
 
-    loadBalancedFargateService.targetGroup.configureHealthCheck({
-      path: "/actuator/health",
+    const container = fargateTaskDefinition.addContainer("app", {
+      containerName: 'coffee-app',
+      image: ecs.ContainerImage.fromEcrRepository(repository),
+      logging: ecs.LogDrivers.awsLogs({streamPrefix: 'eda-ws'}),
+      environment: { 
+        SPRING_PROFILES_ACTIVE: "dev",
+      }
     });
+    container.addPortMappings({
+      containerPort: 8080,
+    });
+
+    const sg_service = new ec2.SecurityGroup(this, 'EdaWSFargateSG', { vpc: vpc });
+    sg_service.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(8080));
+
+    const service = new ecs.FargateService(this, 'EdaWSFargateSvc', {
+      cluster,
+      serviceName: 'eda-ws-svc-coffee',
+      taskDefinition: fargateTaskDefinition,
+      desiredCount: 2,
+      assignPublicIp: false,
+      securityGroups: [sg_service]
+    });
+
+    const scaling = service.autoScaleTaskCount({ maxCapacity: 4, minCapacity: 2 });
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 70,
+      scaleInCooldown: Duration.seconds(60),
+      scaleOutCooldown: Duration.seconds(60)
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'EdaWSALB', {
+      vpc,
+      internetFacing: true,
+      loadBalancerName: 'eda-ws-lb-coffee'
+    });
+
+    const listener = lb.addListener('lb-eda-ws-listener', {
+      port: 80,
+    });
+
+    listener.addTargets('EdaWSTarget', {
+      port: 80,
+      targets: [service],
+      targetGroupName: 'eda-ws-targetgroup',
+      healthCheck: { path: '/actuator/health' }
+    });
+    listener.connections.allowDefaultPortFromAnyIpv4('Allow all traffic');
   }
 }
 
